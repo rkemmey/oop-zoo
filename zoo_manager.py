32f4dd4c659990d7098b87113e29254c00a6873f
rkemmey <rachel.kemmey@gmail.com>
2025-02-12 10:22:09 -0600

    updated

--- zoo_manager.py
+++ zoo_manager.py
@@ -1,5 +1,5 @@
 # zoo manager classes
-class Animal(): 
+class Animal: 
     def __init__(self, name, species):
         self.name = name
         self.species = species
@@ -8,43 +8,36 @@ class Animal():
         return f'Animal makes this sound'
     
 class Mammal(Animal):
-    def __init__(self):
-        super().__init__(self, species='Mammal')
-
     def give_birth(self):
         print(f'{self.name} has given birth')
 
 class Bird(Animal):
-    def __init__(self, wingspan):
-        super().__init__(self, species='Bird', wingspan=0)
+    def __init__(self, name, species, wingspan):
+        super().__init__(name, species='Bird')
         self.wingspan = wingspan
 
 class Reptile(Animal):
-    def __init__(self):
-        super().__init__(self, species='Reptile')
-
     def bask_in_sun(self):
         print(f'{self.name} is basking in the sun.')
 
 class Primate(Mammal):
-    def __init__(self):
-        super().__init__(self)
-
     def climb_trees(self):
         print(f'{self.name} is climbing trees.')
 
 class Marsupial(Mammal):
-    def __init__(self):
-        super().__init__(self)
-
     def carry_baby(self):
         print(f"{self.name} is carrying it's baby.")
 
-class Aviary():
+class Aviary:
     def __init__(self, birds):
-        self.birds = Bird() # stores a list of bird instances
+        self.birds = [] #Bird() # stores a list of bird instances
 
-class ReptileEnclosure():
+class ReptileEnclosure:
     def __init__(self, reptiles):
-        self.reptiles = Reptile() # stores a list of reptile instances
+        self.reptiles = [] #Reptile() # stores a list of reptile instances
         
+toucanSam = Animal('sam', 'toucan')
+print(toucanSam.speak())
+
+snake = Reptile('diamond', 'rattlesnake')
+snake.bask_in_sun()
